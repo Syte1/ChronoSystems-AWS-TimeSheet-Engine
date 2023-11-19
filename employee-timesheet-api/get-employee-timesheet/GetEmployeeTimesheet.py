64e6fdbd0e8475cd3ad2822e58c69d5b0e0ba439
Steven Chow <steven.chow@live.ca>
2023-11-18 17:28:51 -0800

    Add try-catch in lambda handler

--- employee-timesheet-api/get-employee-timesheet/GetEmployeeTimesheet.py
+++ employee-timesheet-api/get-employee-timesheet/GetEmployeeTimesheet.py
@@ -10,31 +10,49 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(TABLE_NAME)
 
 def lambda_handler(event, context):
-    start_date = unquote(event["params"]["querystring"]["start_date"])
-    end_date = unquote(event["params"]["querystring"]["end_date"])
-    uid = event["params"]["querystring"]["uid"]
-    date_list = generateListOfDates(start_date, end_date)
-    
-    valid_date_list = isValidDateList(date_list)
-    
-    if valid_date_list:
-        response = getTimesheetEntries(uid, date_list)
+    try:
+        start_date = unquote(event["params"]["querystring"]["start_date"])
+        end_date = unquote(event["params"]["querystring"]["end_date"])
+        uid = event["params"]["querystring"]["uid"]
+
+        date_list = generateListOfDates(start_date, end_date)
+        valid_date_list = isValidDateList(date_list)
+        
+        if valid_date_list:
+            response = getTimesheetEntries(uid, date_list)
+            return {
+                'statusCode': 200,
+                'entries': json.dumps(response, cls = DecimalEncoder)
+            }
+        else:
+            return {
+                'statusCode': 400,
+                'message': 'Failure, invalid date range.'
+            }
+    except Exception as err:
+        print(f"Error occured in GetEmployeeSheet LambdaHandler: {err}")
         return {
-            'statusCode': 200,
-            'entries': json.dumps(response, cls = DecimalEncoder)
+            'statusCode': 500,
+            'message': "Failure, The server has encountered a situation it does not know how to handle.",
+            'error': "Internal error."
         }
-    else:
-        return {
-            'statusCode': 401,
-            'message': 'Failure, invalid date range.'
-        }
+
     
 def isValidDateList(date_list: list) -> bool:
+    """
+    Check if date list contains more than one date.
+
+    Returns True if there are more than one date, else False.
+    """
     return len(date_list) > 0
     
 def generateListOfDates(start_date: str, end_date: str) -> list:
+    """
+    Generates a list of dates given a range in mm/dd/yyyy format.
+
+    Returns a list of date strings.
+    """
     date_list = []
-    
     date_format = "%m/%d/%Y"
     start_datetime = datetime.strptime(start_date, date_format)
     end_datetime = datetime.strptime(end_date, date_format)
@@ -44,7 +62,12 @@ def generateListOfDates(start_date: str, end_date: str) -> list:
         start_datetime += timedelta(days=1)
     return date_list
     
-def getTimesheetEntries(uid: str, date_list: list):
+def getTimesheetEntries(uid: str, date_list: list) -> list:
+    """
+    Retrieves submissons for dates in a given list.
+
+    Returns a list of submissions in JSON format
+    """
     batch_keys = {
         table.name : {
             'Keys' : [{
