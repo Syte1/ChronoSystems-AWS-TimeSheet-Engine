08383e6b8bcba399c6f6df03066963b43a9ed140
Chun <peterpang1103@gmail.com>
2023-11-27 13:26:11 -0800

    employee payroll lambda and documentations

---
+++ employee-payroll-api/getPayroll.js
@@ -0,0 +1,68 @@
+var AWS = require("aws-sdk");
+var dynamodb = new AWS.DynamoDB();
+var docClient = new AWS.DynamoDB.DocumentClient();
+exports.handler = (event, context, callback) => {
+  console.log(event)
+  const uid = event.params.querystring.uid
+
+  if (event.params.querystring.start_period && event.params.querystring.end_period) {
+    const start_period_end_period = `${event.params.querystring.start_period}_${event.params.querystring.end_period}`
+    getPayrollByPeriod(uid, start_period_end_period, callback)
+  } else{
+    getPayrollById(uid,callback)
+  }
+
+  function getPayrollByPeriod(uid, start_period_end_period, callback) {
+    if (!uid || !start_period_end_period) {
+      return callback(null, JSON.parse({"message": "uid and start_period_end_period are required", "statusCode": 400}))
+    }
+
+    const params = {
+      TableName: "payroll",
+      Key: {
+        uid,
+        start_period_end_period
+      }
+    };
+
+    docClient.get(params, function (err, data) {
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        return callback(null, JSON.parse({"message": "Unable to read item. Error JSON:", "statusCode": 500}))
+      } else {
+        console.log(data)
+        if (!data.Item || data.Item.length === 0){
+          return callback(null, JSON.parse({"message": "No record were found with the parameter provided, please try again", "statusCode": 404}))
+        }
+         return callback(null, JSON.parse({"message": "get payroll succeeded", "Item": data.Item, "statusCode": 200}));
+      }
+    });
+  }
+
+  function getPayrollById(uid, callback) {
+    const params = {
+      TableName: "payroll",
+      KeyConditionExpression: 'uid = :uid',
+      ExpressionAttributeValues: {
+        ':uid': uid,
+      },
+      ScanIndexForward: false,
+    };
+    docClient.query(params, function (err, data) {
+      if (err) {
+        console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
+        return callback(null, JSON.parse({"message": `Unable to read item. Error JSON:${err}`, "statusCode": 500}))
+      } else {
+          if (!data.Items || data.Items.length === 0){
+          return callback(null, JSON.parse({"message": "No record were found with the parameter provided, please try again", "statusCode": 404}))
+        }
+        console.log("Query succeeded:", JSON.stringify(data, null, 2));
+        const items = data.Items
+        if (items.length > 1) {
+          items.sort((a, b) => a.end_date > b.end_date)
+        }
+        return callback(null, JSON.parse({"message": "get payroll succeeded", "Items": items, "statusCode": 200}));
+      }
+    });
+  }
+} 
--- employee-timesheet-api/modify-employee-timesheet/getUnprocessedEntries.js
+++ employee-timesheet-api/modify-employee-timesheet/getUnprocessedEntries.js
@@ -1,37 +1,39 @@
-var AWS = require("aws-sdk");
-var dynamodb = new AWS.DynamoDB();
-var docClient = new AWS.DynamoDB.DocumentClient();
 const axios = require('axios')
 
 exports.handler = async (event, context, callback) => {
     const uid = event.params.querystring.uid
     const payrollApi = `https://s2z6h094ph.execute-api.us-west-2.amazonaws.com/test?uid=${uid}`
+    const timesheetApi = 'https://oxtwzrrqrg.execute-api.us-west-2.amazonaws.com/development/getEmployeeTimesheet'
 
-    const {end_period} = await axios.get(payrollApi)
+    const end_period = await axios.get(payrollApi)
+    console.log(end_period.data);
+    if (end_period.data == "No record were found with the parameter provided, please try again") {
+        console.log("inside if");
+        const data = await axios.get(`${timesheetApi}?uid=${uid}`)
+        return callback(null, JSON.parse(JSON.stringify(data.data, null, 2)))
+       
+    }
 
-    const unprocessedStart = new Date(end_period)
-    const unprocessedEnd = new Date()
+    let unprocessedStart = new Date(end_period.data.end_period)
+    let unprocessedEnd = new Date()
     unprocessedStart.setDate(unprocessedStart.getDate() + 1)
     unprocessedEnd.setDate(unprocessedEnd.getDate() + 14)
 
-    formattedStart = unprocessedStart.toLocaleDateString('en-US', {
+    let formattedStart = unprocessedStart.toLocaleDateString('en-US', {
         month: '2-digit',
         day: '2-digit',
         year: 'numeric'
     })
 
-    formattedEnd = unprocessedEnd.toLocaleDateString('en-US', {
+    let formattedEnd = unprocessedEnd.toLocaleDateString('en-US', {
         month: '2-digit',
         day: '2-digit',
         year: 'numeric'
     })
-
-
-    const timesheetApi = `https://oxtwzrrqrg.execute-api.us-west-2.amazonaws.com/development/getEmployeeTimesheet?uid=${uid}&start_date=${formattedStart}&end_date=${formattedEnd}`
     
-    const data = await axios.get(timesheetApi)
+    const data = await axios.get(`${timesheetApi}?uid=${uid}&start_date=${formattedStart}&end_date=${formattedEnd}`)
 
-    return callback(null, JSON.parse(JSON.stringify(data, null, 2)))
+    return callback(null, JSON.parse(JSON.stringify(data.data, null, 2)))
 
 
 }
