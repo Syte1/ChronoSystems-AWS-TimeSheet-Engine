9506523f7d63a3f6d4b50575e6a91d1ef8643e52
Andy <andy-nguyen@live.ca>
2023-12-02 20:07:14 -0800

    Added Navigation bar to relevant pages

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,23 @@
 const divName = document.getElementById('user-name');
 const testUid = "2";
 
+function initialLoad(){
+    checkSessionStorage();
+    sessionStorage.setItem("uid", "2"); // Ideally, we'd have a cookie that stores the priviledge of user somewhere
+    const currentUser = sessionStorage.getItem("uid");
+    if (currentUser == "2"){
+        document.getElementById("navAdmin").hidden = false;
+
+        // This one is for main landing page, didn't feel like making another js file for this
+        try{
+            document.getElementById("mainAdmin").hidden = false;
+        } catch(_){
+            // do nothing
+        }
+    }
+}
+initialLoad();
+
 //populate name 
 async function checkSessionStorage() {
     const name = sessionStorage.getItem("user-name");
@@ -10,14 +27,16 @@ async function checkSessionStorage() {
         divName.innerText = name;
     }
 }
-checkSessionStorage();
+
 
 async function fetchUsername(){
-    fetch(`https://fqov6b86hi.execute-api.us-east-2.amazonaws.com/v1/profile?uid=${testId}`)
+    fetch(`https://fqov6b86hi.execute-api.us-east-2.amazonaws.com/v1/profile?uid=${testUid}`)
         .then(response => response.json())
         .then(data => {
             sessionStorage.setItem("user-name", data.item.name);
             divName.innerText = data.item.name;
+            console.log(`role: ${data.item.role}`)
         })
-        .catch(error => console.error('Error:', error));
+        .catch(error => {console.error('Error:', error);
+        window.location.replace("../index.html");});
 }
