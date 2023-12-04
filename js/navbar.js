bc4a01c5029fdf78ed1b7c2eaf0cdc17c0f5bb26
Andy <andy-nguyen@live.ca>
2023-12-03 20:01:48 -0800

    working on navabar

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,12 @@
 const divName = document.getElementById('user-name');
-const testUid = "2";
+const baseUrl = 'https://y7aq7em2t6.execute-api.us-west-2.amazonaws.com/test';
+let testUid = "2";
 
-function initialLoad(){
-    checkSessionStorage();
-    sessionStorage.setItem("uid", "2"); // Ideally, we'd have a cookie that stores the priviledge of user somewhere
-    const currentUser = sessionStorage.getItem("uid");
-    if (currentUser == "2"){
+async function initialLoad(){
+    //sessionStorage.setItem("uid", "2"); // Ideally, we'd have a cookie that stores the priviledge of user somewhere
+    //const currentUser = sessionStorage.getItem("uid");
+    const currentUser = await checkSessionStorage().userId;
+    if (currentUser == "shuang" || currentUser == "shuangchun"){
         document.getElementById("navAdmin").hidden = false;
 
         // This one is for main landing page, didn't feel like making another js file for this
@@ -18,13 +19,57 @@ function initialLoad(){
 }
 initialLoad();
 
+// Function to call the API endpoint for token verification
+async function verifyToken() {
+  let token = getSecureCookie();
+  try {
+    const response = await fetch(`${baseUrl}/validate-jwt`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ "id": token }),
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+      const userId = data.body.username;
+
+      return {valid: true, userId: userId, data: data.valid};
+    } else {
+      throw new Error('Token verification failed');
+    }
+  } catch (error) {
+    console.error(error);
+    return {valid: false, userId: null, data: null};
+  }
+}
+
+// Helper function to retrieve the secure cookie value
+function getSecureCookie(cookieName = 'token') {
+  // const cookieName = 'token';
+  let cookies = document.cookie.split(';');
+  let cookie = cookies.find(cookie => cookie.startsWith(cookieName));
+  if (cookie) {
+    return cookie.split('=')[1];
+  } else {
+    return null;
+  }
+}
+
 //populate name 
 async function checkSessionStorage() {
     const name = sessionStorage.getItem("user-name");
+    sessionStorage.setItem("user-name", "");
     if (name == undefined || name == ""){
+        const response = await verifyToken();
+        testUid = response.userId;
         await fetchUsername();
+        return response;
     } else {
         divName.innerText = name;
+        return name;
     }
 }
 
@@ -38,5 +83,6 @@ async function fetchUsername(){
             console.log(`role: ${data.item.role}`)
         })
         .catch(error => {console.error('Error:', error);
-        window.location.replace("../index.html");});
+        //window.location.replace("../index.html");
+    });
 }
